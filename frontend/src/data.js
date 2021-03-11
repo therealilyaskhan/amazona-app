e7affea7da1470f8428a43be753b66ba6de13465
therealilyaskhan <therealilyaskhan@hotmail.com>
2021-03-11 15:02:54 +0500

    First Commit Amazona App

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Nike Slim Shirt',
+      category: 'Shirts',
+      image: '../img/p1.jpg',
+      price: '129.00',
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 12,
+      description: 'A very good quality shirt!!!'
+    },
+    {
+      _id: '2',
+      name: 'Adidas Slim Fit Shirt',
+      category: 'Shirts',
+      image: '../img/p2.jpg',
+      price: '160.00',
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 8,
+      description: 'Good quality available in all sizes'
+    },
+    {
+      _id: '3',
+      name: 'Lacose Free Shirt',
+      category: 'Shirts',
+      image: '../img/p3.jpg',
+      price: '65.00',
+      brand: 'Lacose',
+      rating: 3.5,
+      numReviews: 10,
+      description: 'A free size shirt'
+    },
+    {
+      _id: '4',
+      name: 'Nike Slim Pant',
+      category: 'Pants',
+      image: '../img/p4.jpg',
+      price: '78.00',
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'A very good quality shirt!!!'
+    },
+    {
+      _id: '5',
+      name: 'Puma Slim Pant',
+      category: 'Pants',
+      image: '../img/p5.jpg',
+      price: '65.00',
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    },
+    {
+      _id: '6',
+      name: 'Adidas Fit Pant',
+      category: 'Pants',
+      image: '../img/p6.jpg',
+      price: '139.00',
+      brand: 'Adidas',
+      rating: 5.0,
+      numReviews: 25,
+      description: 'Best quality pants and shirt!!!'
+    }
+  ]
+};
+
+export default data;
