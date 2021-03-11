a89cbcb27dc741f72551d77db8bba305b58cc42b
therealilyaskhan <therealilyaskhan@hotmail.com>
2021-03-12 00:10:08 +0500

    Minimalist React App Strcuture 12/3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
+//The react application's entry point;
+//here importing the react and react-dom from the node_modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+//and here we don't say import dash from dash this is because we are not actually importing the css file but this line is only here to instruct the webpack bundler;
 import './index.css';
+//remember in a react application it is only one single ReactDOM.render() call at the end of the day that is going to mount a single react functional component called App on a 'root' container inside the html markup file; and from inside of that function react component called "App" we are going to refer to all other different components as its children;
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
